eddf4e8a66d01996343ad0211bf236d0016fea50
slepzov <slepzov@yandex.ru>
2022-08-01 22:47:03 +0300

    add start info

--- script.py
+++ script.py
@@ -1,9 +1,58 @@
 import tarfile
 import sys
+import os
 
-file_name = sys.argv[1]
+SNAP_NAME = sys.argv[1]
+SERIAL_NUMBER = ""
+DIRECTORY_NAME = SNAP_NAME[:-4]
+CODE_LEVEL = ""
 
 if __name__ == "__main__":
-    with tarfile.open(file_name) as tar:
-        tar.extractall(path=file_name[:-4])
+    with tarfile.open(SNAP_NAME) as tar:
+        tar.extractall(path=DIRECTORY_NAME)
 
+INTERNALSTORAGE = ""
+SAOUT = ""
+MACHINENODEINFO = ""
+
+files = os.listdir(DIRECTORY_NAME + "\dumps")
+for file_name in files:
+    if "internalstorage" in file_name:
+        INTERNALSTORAGE = file_name
+    if "saout" in file_name:
+        SAOUT = file_name
+    if "machinenodeinfo" in file_name:
+        MACHINENODEINFO = file_name
+
+PRODUCT_NAME = ""
+with open(DIRECTORY_NAME + "\dumps\\" + SAOUT) as f:
+    for line in f:
+        if "product_name" in line:
+            PRODUCT_NAME = line[13:]
+            break
+
+TYPE = ""
+
+with open(DIRECTORY_NAME + "\dumps\\" + INTERNALSTORAGE) as f:
+    for line in f:
+        if ":control:" in line:
+            TYPE = line.split(":")[6]
+            SERIAL_NUMBER = line.split(":")[7]
+            break
+
+with open(DIRECTORY_NAME + "\dumps\\" + MACHINENODEINFO) as f:
+    for line in f:
+        if "config node" in line:
+            CODE_LEVEL = line.split(",")[8].split(" ")[0]
+            break
+
+date_timestamp = SAOUT.split(".")[-2][4:] + "." + SAOUT.split(".")[-2][2:4] + "." + "20" + SAOUT.split(".")[-2][0:2]
+time_timestamp = SAOUT.split(".")[-1][0:2] + ":" + SAOUT.split(".")[-1][2:4] + ":" + SAOUT.split(".")[-1][4:]
+
+print(f"Главный файл = {INTERNALSTORAGE}")
+print(f"saout файл = {SAOUT}")
+print(f"Product_name: {PRODUCT_NAME}")
+print(f"Type: {TYPE}")
+print(f"Серийный номер СХД: {SERIAL_NUMBER}")
+print(f"Code level: {CODE_LEVEL}")
+print(f"Timestamp: {date_timestamp} {time_timestamp}")
